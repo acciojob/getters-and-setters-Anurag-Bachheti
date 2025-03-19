78a424ee255933b4608b140223e3a30cec6a80f1
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-19 13:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-//complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this._name;
+	}
 
-class Teacher extends Person {}
+    set name(newName){
+        if(newName.length > 0){
+            this._name = newName;
+        } else {
+            console.log("Name cannot be empty!");     
+        }
+    }
+
+    get age(){
+        return this._age;
+    }
+
+	set age(newAge){
+        if(newAge > 0){
+            this._age = newAge;
+        } else {
+            console.log("Age must be positive number!")
+        }
+	}
+}
+
+class Student extends Person {
+	constructor(name, age){
+		super(name,age)
+	}
+	study(){
+		console.log(`${this._name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name, age){
+		super(name, age)
+	}
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+
 
 // Do not change the code below this line
 window.Person = Person;
